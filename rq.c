fdad238fc18cc324205fdb25572b5f2b1c582fc7
Andy Rechenberg <github@rechenberg.net>
2023-07-13 22:59:57 +0000

    program to remove non-alphanumeric characters from string.

---
+++ rq.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+char *stringRemoveNonAlphaNum(char *str)
+{
+	unsigned long i = 0;
+	unsigned long j = 0;
+	char c;
+	char *tmp = malloc(sizeof(str) + 1);
+
+        while ((c = str[i]) != '\0') {
+                if (isalnum(c)) {
+                        tmp[j] = str[i];
+                        i++;
+                        j++;
+                } else {
+                        i++;
+                }
+        }
+
+	tmp[j] = '\0';
+
+	return tmp;
+}
+
+int main(void) {
+
+	char *z = "\"flux\"";
+
+	char *alphanum = stringRemoveNonAlphaNum(z);
+
+	printf("no special chars: %s\n", alphanum);
+
+	free(alphanum);
+
+
+	return 0;
+}
